309cb7aa0fef9e2c15b7aafd145fdc685e56fe13
Sergii.Pivtorak <sepivtorak@deloitte.com>
2020-01-10 15:34:13 +0200

    Updates

---
+++ precache-manifest.fc3debd230cf0f653374605d404716c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6e0c5023f8469a9da70332dbc4df16",
+    "url": "/RN_Universal/index.html"
+  },
+  {
+    "revision": "0148b0dd0b842d37d4cd",
+    "url": "/RN_Universal/static/js/2.372b0dae.chunk.js"
+  },
+  {
+    "revision": "1e075dc398ed2e9ecbd498bb822be0da",
+    "url": "/RN_Universal/static/js/2.372b0dae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fcb3e047c7ed82a3e2e",
+    "url": "/RN_Universal/static/js/main.93dd93c4.chunk.js"
+  },
+  {
+    "revision": "81f167c9a525c8742415",
+    "url": "/RN_Universal/static/js/runtime-main.d84b2eff.js"
+  },
+  {
+    "revision": "abe0e6fe996ff3f289db51a1e910355f",
+    "url": "/RN_Universal/static/media/login_logo.abe0e6fe.png"
+  }
+]);
